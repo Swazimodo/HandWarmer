4b2ebf2b72c6d81d65a1649bf66cad0ab671b9aa
sam nesbitt <sam.w.nesbitt@gmail.com>
2017-12-28 20:30:12 -0700

    Created a simple load generator to make my laptop stop sipping power when I want to warm up my hands

---
+++ HandWarmer/ControlForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace HandWarmer
+{
+    partial class ControlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLevel = new System.Windows.Forms.TrackBar();
+            this.lMin = new System.Windows.Forms.Label();
+            this.lMax = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbLevel
+            // 
+            this.tbLevel.LargeChange = 2;
+            this.tbLevel.Location = new System.Drawing.Point(12, 12);
+            this.tbLevel.Maximum = 8;
+            this.tbLevel.Name = "tbLevel";
+            this.tbLevel.Size = new System.Drawing.Size(284, 56);
+            this.tbLevel.TabIndex = 2;
+            this.tbLevel.Scroll += new System.EventHandler(this.tbLevel_Scroll);
+            // 
+            // lMin
+            // 
+            this.lMin.AutoSize = true;
+            this.lMin.Location = new System.Drawing.Point(12, 43);
+            this.lMin.Name = "lMin";
+            this.lMin.Size = new System.Drawing.Size(16, 17);
+            this.lMin.TabIndex = 3;
+            this.lMin.Text = "0";
+            // 
+            // lMax
+            // 
+            this.lMax.AutoSize = true;
+            this.lMax.Location = new System.Drawing.Point(280, 43);
+            this.lMax.Name = "lMax";
+            this.lMax.Size = new System.Drawing.Size(16, 17);
+            this.lMax.TabIndex = 4;
+            this.lMax.Text = "8";
+            // 
+            // ControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 69);
+            this.Controls.Add(this.lMax);
+            this.Controls.Add(this.lMin);
+            this.Controls.Add(this.tbLevel);
+            this.Name = "ControlForm";
+            this.Text = "Simple Hand Warmer";
+            this.Load += new System.EventHandler(this.ControlForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbLevel;
+        private System.Windows.Forms.Label lMin;
+        private System.Windows.Forms.Label lMax;
+    }
+}
+
---
+++ HandWarmer/ControlForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace HandWarmer
+{
+    public partial class ControlForm : Form
+    {
+        Stack<Tuple<Task, CancellationTokenSource>> _tasks = new Stack<Tuple<Task, CancellationTokenSource>>();
+        //List<Tuple<Task, CancellationTokenSource>> _tasks = new List<Tuple<Task, CancellationTokenSource>>();
+
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// update slider bar based on current core count
+        /// </summary>
+        private void ControlForm_Load(object sender, EventArgs e)
+        {
+            lMax.Text = Environment.ProcessorCount.ToString();
+            tbLevel.Maximum = Environment.ProcessorCount;
+        }
+
+        /// <summary>
+        /// update CPU work load
+        /// </summary>
+        private async void tbLevel_Scroll(object sender, EventArgs e)
+        {
+            if(tbLevel.Value > _tasks.Count)
+            {
+                //add more cancellable tasks
+                var ts = new CancellationTokenSource();
+                CancellationToken ct = ts.Token;
+                Task t = Task.Factory.StartNew(() =>
+                {
+                    while (true)
+                    {
+                        //Check if cancel was requested
+                        if (ct.IsCancellationRequested)
+                            break;
+                    }
+                }, ct);
+
+                _tasks.Push(new Tuple<Task, CancellationTokenSource>(t, ts));
+            }
+            else if(tbLevel.Value < _tasks.Count)
+            {
+                //cancel tasks
+                Tuple<Task, CancellationTokenSource> workItem = _tasks.Pop();
+                workItem.Item2.Cancel();
+                await workItem.Item1;
+            }
+        }
+    }
+}
