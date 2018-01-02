2f235677523f39c1204c0e60fc1074c27ca3617e
Sam Nesbitt <sam.w.nesbitt@gmail.com>
2018-01-02 15:01:43 -0700

    Widened the form slightly so the name was not truncated

--- HandWarmer/ControlForm.Designer.cs
+++ HandWarmer/ControlForm.Designer.cs
@@ -36,41 +36,47 @@
             // 
             // tbLevel
             // 
+            this.tbLevel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tbLevel.LargeChange = 2;
-            this.tbLevel.Location = new System.Drawing.Point(12, 12);
+            this.tbLevel.Location = new System.Drawing.Point(9, 10);
+            this.tbLevel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbLevel.Maximum = 8;
             this.tbLevel.Name = "tbLevel";
-            this.tbLevel.Size = new System.Drawing.Size(284, 56);
+            this.tbLevel.Size = new System.Drawing.Size(223, 45);
             this.tbLevel.TabIndex = 2;
             this.tbLevel.Scroll += new System.EventHandler(this.tbLevel_Scroll);
             // 
             // lMin
             // 
             this.lMin.AutoSize = true;
-            this.lMin.Location = new System.Drawing.Point(12, 43);
+            this.lMin.Location = new System.Drawing.Point(9, 35);
+            this.lMin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lMin.Name = "lMin";
-            this.lMin.Size = new System.Drawing.Size(16, 17);
+            this.lMin.Size = new System.Drawing.Size(13, 13);
             this.lMin.TabIndex = 3;
             this.lMin.Text = "0";
             // 
             // lMax
             // 
             this.lMax.AutoSize = true;
-            this.lMax.Location = new System.Drawing.Point(280, 43);
+            this.lMax.Location = new System.Drawing.Point(210, 35);
+            this.lMax.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lMax.Name = "lMax";
-            this.lMax.Size = new System.Drawing.Size(16, 17);
+            this.lMax.Size = new System.Drawing.Size(13, 13);
             this.lMax.TabIndex = 4;
             this.lMax.Text = "8";
             // 
             // ControlForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 69);
+            this.ClientSize = new System.Drawing.Size(241, 56);
             this.Controls.Add(this.lMax);
             this.Controls.Add(this.lMin);
             this.Controls.Add(this.tbLevel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "ControlForm";
             this.Text = "Simple Hand Warmer";
